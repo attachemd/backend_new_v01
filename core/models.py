9ec9a80cace966f0584c691f1b98593c285e21a7
attachemd <mattache.vivace@gmail.com>
2021-11-12 09:57:24 +0100

    Added recipe model.

--- core/models.py
+++ core/models.py
@@ -45,7 +45,7 @@ class Tag(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
-    objects = BaseUserManager()
+    # objects = BaseUserManager()
 
     def __str__(self):
         return self.name
@@ -58,8 +58,26 @@ class Ingredient(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
-    objects = BaseUserManager()
+    # objects = BaseUserManager()
 
     def __str__(self):
         return self.name
 
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    # objects = BaseUserManager()
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+    # image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+
+    def __str__(self):
+        return self.title
