01f8fd22e59d97570218c90107c490bc23920604
attachemd <mattache.vivace@gmail.com>
2021-12-08 16:43:14 +0100

    Project updated.

--- user/urls.py
+++ user/urls.py
@@ -6,6 +6,7 @@ app_name = 'user'
 
 urlpatterns = [
     path('create/', views.CreateUserView.as_view(), name='create'),
+    # path('create/', views.registration_view, name='create'),
     path('access/', views.MyTokenObtainPairView.as_view(), name='access'),
     path('me/', views.ManageUserView.as_view(), name='me'),
 ]
--- user/views.py
+++ user/views.py
@@ -1,13 +1,46 @@
-from rest_framework import generics, authentication
+from rest_framework import generics, authentication, status
+from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
+from selenium.webdriver.common.devtools.v85.network import Response
+
 from user.serializers import UserSerializer, MyTokenObtainPairSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSerializer
+
+
+# @api_view(['POST', ])
+# def registration_view(request):
+#     if request.method == 'POST':
+#         serializer = UserSerializer(data=request.data)
+#
+#         data = {}
+#
+#         if serializer.is_valid():
+#             account = serializer.save()
+#
+#             data['response'] = "Registration Successful!"
+#             # data['username'] = account.username
+#             data['email'] = account.email
+#
+#             # token = Token.objects.get(user=account).key
+#             # data['token'] = token
+#
+#             refresh = RefreshToken.for_user(account)
+#             data['token'] = {
+#                 'refresh': str(refresh),
+#                 'access': str(refresh.access_token),
+#             }
+#
+#         else:
+#             data = serializer.errors
+#
+#         return Response(data, status=status.HTTP_201_CREATED)
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
